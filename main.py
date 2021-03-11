bbabc734aa204427d2ada1fe333dbb101e216419
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-03-10 22:12:11 -0800

    update #1

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+def pass_gen():
+    print('FIXME: pass_gen')
+    # ASCII A-Z = 65 --> 90
+    # ASCII a-z = 97 --> 122
+    # ASCII Symbols: '!': 33, '# $ % &': 35 --> 38 
+    chars = ['']
+    num_chars = random.randint(8, 20)
+
+    return -1
+
+
+def main():
+    user_input = ''
+    while user_input != '0':
+        pass_gen()
+        user_input = input('Press enter to regen. 0 to quit\n')
+    return -1
+
+main()
+
+# randomizer: mix lowercase, uppercase, nums and symbs
